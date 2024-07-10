fa7270224c1a4cc3ca3470f252212287c42388dc
Shuja Ali <shujaali1234@gmail.com>
2024-07-11 03:25:38 +0500

    Updated code for cloudinary AI and logo

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="backdropai"
       options={{
         multiple: false,
         resourceType: "image",
